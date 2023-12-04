8b6af760a36c973c870b348ffdcad74294ec1f94
Martin Martinez <mtz.martin@proton.me>
2023-12-04 15:08:21 -0600

    This is the basic UI, once it works I will add more things :D

--- QuotableApp/ContentView.swift
+++ QuotableApp/ContentView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 16)
+                .frame(height: 650)
+                .foregroundColor(.indigo)
+                .overlay(
+                    VStack {
+                        Text("If opportunity doesn't knock, build a door.")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .bold()
+                            .padding(.horizontal)
+                    }
+                )
+            Button {
+                //code here
+            } label: {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(.clear)
+                    .frame(width: 150, height: 50)
+                    .overlay(
+                        Text("New Quote")
+                            .foregroundColor(.black)
+                            .bold()
+                            .font(.title2)
+                    )
+            }
         }
         .padding()
     }
