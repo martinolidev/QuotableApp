0e6045da044bec1633b6db04e7677281ee83040d
Martin Martinez <mtz.martin@proton.me>
2023-12-04 15:36:25 -0600

    My basic api call, I need to learn more things

--- QuotableApp/QuoteModel.swift
+++ QuotableApp/QuoteModel.swift
@@ -7,7 +7,32 @@
 
 import Foundation
 
-struct Quote: Codable {
+struct QuoteContent: Codable {
     let content: String
     let author: String
+}
+
+class QuoteModel {
+    
+    func fetchQuote() {
+        guard let url = URL(string: "https://api.quotable.io/random") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, error, response) in
+            
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    let quote = try decoder.decode(QuoteContent.self, from: data)
+                    print(quote.content)
+                } catch {
+                    print("Error to decode data")
+                }
+            }
+        }
+        task.resume()
+    }
+    
 }
