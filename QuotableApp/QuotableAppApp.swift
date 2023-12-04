b3b2aa273ed1ae4e37fcd946b0f7de896f3b28f4
Martin Martinez <mtz.martin@proton.me>
2023-12-04 14:47:06 -0600

    Initial Commit

---
+++ QuotableApp/QuotableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotableAppApp.swift
+//  QuotableApp
+//
+//  Created by Martin Martinez on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct QuotableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
